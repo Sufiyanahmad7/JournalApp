89dcea9df36c86b730566f511b046889a31bda69
Sufiyanahmad7 <ahmadsufiyan470@gmail.com>
2024-08-01 20:34:48 +0530

    first commit

---
+++ src/test/java/com/springboot/JournalApp/JournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.JournalApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
