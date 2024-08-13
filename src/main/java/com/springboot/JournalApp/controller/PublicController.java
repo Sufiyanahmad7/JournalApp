c3a1714c5d35b8991e86c0167176cbf8aabc03d2
Sufiyanahmad7 <ahmadsufiyan470@gmail.com>
2024-08-13 16:22:48 +0530

    Added PostMapping create-user

--- src/main/java/com/springboot/JournalApp/controller/PublicController.java
+++ src/main/java/com/springboot/JournalApp/controller/PublicController.java
@@ -3,6 +3,8 @@ package com.springboot.JournalApp.controller;
 import com.springboot.JournalApp.entity.User;
 import com.springboot.JournalApp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,7 +21,15 @@ public class PublicController {
 
     }
         @PostMapping("/create-user")
-        public void createUser(@RequestBody User user){
-            userService.saveNewEntry(user);
+        public ResponseEntity<String> createUser(@RequestBody User user) {
+            try {
+                userService.saveNewEntry(user);
+                return new ResponseEntity<>("User registered successfully", HttpStatus.CREATED);
+            } catch (Exception e) {
+                return new ResponseEntity<>("Error: " + e.getMessage(), HttpStatus.BAD_REQUEST);
+            }
+//        public void createUser(@RequestBody User user){
+//
+//            userService.saveNewEntry(user);
     }
 }
